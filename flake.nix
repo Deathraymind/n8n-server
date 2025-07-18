e82fab374716c10ed56ffcb55510a8b242b86897
Deathraymind <deathraymind@gmail.com>
2025-07-18 21:29:47 +0900

    idk

--- flake.nix
+++ flake.nix
@@ -1,19 +1,23 @@
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  flake-utils.url = "github:numtide/flake-utils";
-  agenix.url = "github:ryantm/agenix";
-};
+{
+  description = "NixOS Docker Host";
 
-outputs = { self, nixpkgs, flake-utils, agenix, ... }: {
-  nixosConfigurations.docker-host = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      ./hosts/docker-host.nix
-      ./hosts/containers/caddy.nix
-      /etc/nixos/configuration.nix
-      agenix.nixosModules.default
-    ];
-    specialArgs = { inherit agenix; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    agenix.url = "github:ryantm/agenix";
   };
-};
+
+  outputs = { self, nixpkgs, flake-utils, agenix, ... }:
+    {
+      nixosConfigurations.docker-host = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/docker-host.nix
+          /etc/nixos/configuration.nix
+          agenix.nixosModules.default
+        ];
+        specialArgs = { inherit agenix; };
+      };
+    };
+}
 
