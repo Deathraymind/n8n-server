807cccfd3fa7b7797c515ad592a886670a7fd307
Deathraymind <deathraymind@gmail.com>
2025-07-18 22:05:46 +0900

    jelly intel driver

--- hosts/containers/jellyfin.nix
+++ hosts/containers/jellyfin.nix
@@ -4,5 +4,16 @@
 services.jellyfin = {
     enable = true;
     openFirewall = true;
+    extraPackages = with pkgs; [
+    intel-media-driver
+  ];
   }; 
+hardware.opengl = {
+  enable = true;
+  extraPackages = with pkgs; [
+    intel-media-driver   # Intel's official VAAPI driver (recommended for newer Xeons)
+    libva-utils           # For testing VAAPI (optional)
+  ];
+}; 
 }
+
