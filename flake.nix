15786a099d9a0efeeb7054026315401da0221366
Deathraymind <deathraymind@gmail.com>
2025-07-18 19:32:22 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "NixOS Docker Host";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    {
+      nixosConfigurations.docker-host = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/docker-host.nix
+        ];
+      };
+    };
+}
+
---
+++ hosts/docker-host.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.docker = {
+  enable = true;
+  # Set up resource limits
+  daemon.settings = {
+    experimental = true;
+    default-address-pools = [
+      {
+        base = "172.30.0.0/16";
+        size = 24;
+      }
+    ];
+  };
+};
+
+  users.users.bowyn = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-compose
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 2375 ]; # Optional: expose Docker API (insecure!)
+}
+
