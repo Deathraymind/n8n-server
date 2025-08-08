24ab42cb116f4efb990058902ee70e268433350c
Deathraymind <deathraymind@gmail.com>
2025-08-08 12:26:22 +0900

    back to no gpt

--- services/postgress/postgress.nix
+++ services/postgress/postgress.nix
@@ -1,13 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  services.postgresql = {
+  config.services.postgresql = {
     enable = true;
     ensureDatabases = [ "mydatabase" ];
-    authentication = pkgs.lib.mkOverride 10 [
-      # type  database  user   auth-method
-      { type = "local"; database = "all"; user = "all"; method = "trust"; }
-    ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
   };
 }
-
