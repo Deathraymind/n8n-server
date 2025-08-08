e6596017757a5dc41642abf2fe94ee86699ddd34
Deathraymind <deathraymind@gmail.com>
2025-08-08 12:36:21 +0900

    password auth

--- services/postgress/postgress.nix
+++ services/postgress/postgress.nix
@@ -8,6 +8,12 @@
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
+      host  all       all     127.0.0.1/32   trust
+      host  all       all     ::1/128        trust
     '';
+    initialScript = pkgs.writeText "init.sql" ''
+        ALTER USER postgres WITH PASSWORD '6255';
+    '';
+
   };
 }
