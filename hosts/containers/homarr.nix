463ce6523e4d70306246e0ade9b47363db1c9d59
Deathraymind <deathraymind@gmail.com>
2025-08-14 08:52:53 +0900

    homarr

---
+++ hosts/containers/homarr.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/homarr/appdata 0755 1000 1000 - -"
+  ];
+
+ virtualisation.oci-containers = {
+  backend = "docker";
+  containers.n8n = {
+    image = "ghcr.io/homarr-labs/homarr:latest";        # or pin a version/digest
+    autoStart = true;
+    ports = [ "7575:7575" ];
+    volumes = [ "/var/lib/homarr/appdata:/appdata" ];
+    extraOptions = [ "--pull=always" ]; # re-pull on restart
+    environment = {
+      GENERIC_TIMEZONE = "Asia/Tokyo";
+      # optional: TZ = "Asia/Tokyo";
+    };
+  };
+}; 
+   networking.firewall = {
+    allowedTCPPorts = [ 7575 ]; 
+    allowedUDPPorts = [ 7575 ];
+    };
+}
