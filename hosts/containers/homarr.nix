22dab69e0059fbe86dc6af5ee3534d072559a2be
Deathraymind <deathraymind@gmail.com>
2025-08-14 08:54:41 +0900

    oci homarr container

--- hosts/containers/homarr.nix
+++ hosts/containers/homarr.nix
@@ -8,7 +8,7 @@
 
  virtualisation.oci-containers = {
   backend = "docker";
-  containers.n8n = {
+  containers.homarr = {
     image = "ghcr.io/homarr-labs/homarr:latest";        # or pin a version/digest
     autoStart = true;
     ports = [ "7575:7575" ];
