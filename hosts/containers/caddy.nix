e97b99aa85c3d17fe89c4ad7dcb5f37a105e1b4a
Deathraymind <deathraymind@gmail.com>
2025-09-27 22:12:50 +0900

    rem

--- hosts/containers/caddy.nix
+++ hosts/containers/caddy.nix
@@ -55,7 +55,7 @@
       }
     '';
     virtualHosts."nodejp.deathraymind.net".extraConfig = ''
-      reverse_proxy https://192.168.1.135:8080
+      reverse_proxy http://192.168.1.135:8080
       tls /var/lib/acme/deathraymind.net/cert.pem /var/lib/acme/deathraymind.net/key.pem {
         protocols tls1.3
       }
