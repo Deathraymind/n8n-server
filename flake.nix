69c5b11b01e402a0fa77e3a3f738183e90e40e0b
Deathraymind <deathraymind@gmail.com>
2026-06-30 21:54:25 +0900

    sylvaths flake

--- flake.nix
+++ flake.nix
@@ -15,6 +15,14 @@
     # =============================
     # PHYSICAL NODES
     # =============================
+    nixosConfigurations.node-sylvath = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/node-sylvath/default.nix
+        inputs.sops-nix.nixosModules.sops
+      ];
+      specialArgs = {inherit inputs;};
+    };
     nixosConfigurations.node1 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -47,6 +55,20 @@
       ];
       specialArgs = {inherit inputs;};
     };
+    nixosConfigurations.caddy-sylvath = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/caddy-sylvath/configuration.nix
+        ./hosts/caddy-sylvath/networking.nix
+        ./modules/hardware-configuration.nix # Include our rewritten hardware file
+        ./modules/common.nix # Include our rewritten hardware file
+
+        inputs.sops-nix.nixosModules.sops
+        # This block instructs Nix to build a generic VHD image layout
+      ];
+      specialArgs = {inherit inputs;};
+    };
+
     nixosConfigurations.nas = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -84,6 +106,20 @@
         ./modules/common.nix
         ./hosts/pelican-wings/configuration.nix
         ./hosts/pelican-wings/networking.nix
+        inputs.sops-nix.nixosModules.sops
+        # Proxmox specific configuration (Replaced hardware-configuration.nix)
+      ];
+      specialArgs = {inherit inputs;};
+    };
+    nixosConfigurations.pelican-sylvath-wings = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        inputs.pelican.nixosModules.default
+        {nixpkgs.overlays = [inputs.pelican.overlays.default];}
+        ./modules/hardware-configuration.nix
+        ./modules/common.nix
+        ./hosts/pelican-sylvath-wings/configuration.nix
+        ./hosts/pelican-sylvath-wings/networking.nix
         inputs.sops-nix.nixosModules.sops
         # Proxmox specific configuration (Replaced hardware-configuration.nix)
       ];
